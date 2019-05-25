c39af3602360664bac7b80d794d8ec2743f7f197
parthparikh <parthparikh1999p@gmail.com>
2019-05-25 15:07:16 +0530

    fixed the popup position bug
    P.S the weirdest bug I have encountered to this date!

--- popup/popup.js
+++ popup/popup.js
@@ -1,29 +1,50 @@
-function getSelectedText() {
-	var text = "";
+function getSelectedTextObj() {
+	var textObj;
 	if (typeof window.getSelection != "undefined") {
-		text = window.getSelection().toString();
+		textObj = window.getSelection();
 	} else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
-		text = document.selection.createRange().text;
+		textObj = document.selection.createRange().text;
 	}
-	return text;
+	return textObj;
 }
 
-function doSomethingWithSelectedText() {
-	var selectedText = getSelectedText();
-	if (selectedText && selectedText.split(" ").length <= 2) {
-		var NewPara = document.createElement("p");
+function doSomethingWithSelectedText(obj) {
+	var textObj = getSelectedTextObj();
+	if (textObj.toString() && textObj.toString().trim().split(" ").length <= 2) {
+		var NewPara = document.createElement("div");
 		NewPara.setAttribute("id", "infoDiv");
 		NewPara.setAttribute("class", "tooltipDiv");
-		NewPara.appendChild(document.createTextNode(selectedText));
+		NewPara.appendChild(document.createTextNode(textObj.toString().trim()));
 
 		if (document.getElementsByClassName("tooltipDiv").length) {
 			var OldPara = document.getElementById("infoDiv");
-			document.body.replaceChild(NewPara, OldPara);
+			document.documentElement.replaceChild(NewPara, OldPara);
 		}
 
 		else {
-			document.body.appendChild(NewPara);
+			document.documentElement.appendChild(NewPara);
 		}
+
+		var oRect = textObj.getRangeAt(0).getBoundingClientRect();
+		var leftOffset, topOffset;
+		var pageWidth, pageHeight;
+		var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0],
+
+		pageWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+		pageHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+		leftOffset = oRect.left + oRect.width + 1 + 12;
+		if (300 + leftOffset > pageWidth - 12) {
+			leftOffset = pageWidth - 350 - 12;
+		}
+
+		topOffset = oRect.top;
+		if (topOffset + 12 > pageHeight) {
+			topOffset -= 12;
+		}
+
+		leftOffset += window.scrollX;
+		topOffset += window.scrollY;
 
 		document.getElementById("infoDiv").style.display = "block";
 		document.getElementById("infoDiv").style.width = "250px";
@@ -34,15 +55,8 @@ function doSomethingWithSelectedText() {
 		document.getElementById("infoDiv").style.borderRadius = "0px 0px 25px 0px";
 		document.getElementById("infoDiv").style.position = "absolute";
 
-		if(event.clientX + 300 > document.documentElement.clientWidth) {
-			document.getElementById("infoDiv").style.left = event.clientX - 300;
-		}
-
-		else {
-			document.getElementById("infoDiv").style.left = event.clientX;
-		}
-
-		document.getElementById("infoDiv").style.top = event.clientY;	
+		document.getElementById("infoDiv").style.left = String(leftOffset) + "px";
+		document.getElementById("infoDiv").style.top = String(topOffset) + "px";
 	}
 
 	else {
